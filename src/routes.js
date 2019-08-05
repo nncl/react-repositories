b84d85409429ce98285a3225c74fcbe621fb17dc
Cauê Almeida <nncl@live.com>
2019-08-05 20:06:27 -0300

    Fix prettier

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Switch,
-  Route
-} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Main from './pages/Main';
 import Repository from './pages/Repository';
 
-export default function () {
+export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
