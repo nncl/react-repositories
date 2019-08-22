2c7fc3cd26c4f90071bd525f2e424355c36adc1b
= <nncl@live.com>
2019-08-21 22:31:56 -0300

    Adding styles to Main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import {Title} from './styles';
+import {FaGithubAlt, FaPlus} from 'react-icons/fa';
+import {Container, Form, SubmitButton} from './styles';
 
 export default function Main() {
-  return <Title>Hello World</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositories
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Add repository" />
+
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
