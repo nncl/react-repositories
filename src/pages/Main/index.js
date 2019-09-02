6c0a541273b04f2ce219fffe322c06bbe56f1539
= <nncl@live.com>
2019-09-02 19:59:59 -0300

    Adding/retrieving list to/from localstorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,6 +10,21 @@ export default class Main extends Component {
     loading: false,
   };
 
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+    if (repositories) {
+      this.setState({repositories: JSON.parse(repositories)});
+    }
+  }
+
+  // Update localstorage
+  componentDidUpdate(_, prevState) {
+    const {repositories} = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   handleInputChange = e => {
     this.setState({newRepo: e.target.value});
   };
