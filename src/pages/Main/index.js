6630463229ae1b33aedb49487650792626604544
= <nncl@live.com>
2019-09-02 16:43:46 -0300

    Requesting repo from github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {FaGithubAlt, FaPlus} from 'react-icons/fa';
+import api from '../../services/api';
 import {Container, Form, SubmitButton} from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositories
-      </h1>
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+  };
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Add repository" />
+  handleInputChange = e => {
+    this.setState({newRepo: e.target.value});
+  };
 
-        <SubmitButton>
-          <FaPlus color="#fff" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleFormSubmit = async e => {
+    e.preventDefault();
+    const {newRepo, repositories} = this.state;
+    const response = await api.get(`/repos/${newRepo}`);
+    const data = {
+      name: response.data.full_name,
+    };
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+    });
+  };
+
+  render() {
+    const {newRepo} = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleFormSubmit}>
+          <input type="text"
+                 placeholder="Add repository"
+                 value={newRepo}
+                 onChange={this.handleInputChange} />
+
+          <SubmitButton>
+            <FaPlus color="#fff" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
